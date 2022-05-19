3cefe71983fc30d5d8569e3a43dc3c345112a86c
unknown <facundoromani90@gmail.com>
2022-05-19 16:24:14 -0300

    Primera parte de la App

---
+++ AppCompras/AppCompras/Conexiones/Cconexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+
+namespace AppCompras.Conexiones
+{
+    public class Cconexion
+    {
+        public static FirebaseClient firebase = new FirebaseClient("https://appcompras-5baa3-default-rtdb.firebaseio.com");
+    }
+}
---
+++ AppCompras/AppCompras/Datos/Dproductos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using AppCompras.Modelo;
+using AppCompras.Conexiones;
+
+namespace AppCompras.Datos
+{
+    public class Dproductos
+    {
+        public async Task<List<Mproductos>> Mostrarproductos()
+        {
+            return (await Cconexion.firebase
+                .Child("Productos")
+                .OnceAsync<Mproductos>()).Select(item => new Mproductos
+                {
+                    Descripcion = item.Object.Descripcion,
+                    Icono = item.Object.Icono,
+                    Precio = item.Object.Precio,
+                    Peso = item.Object.Peso,
+                    Idproducto = item.Key,
+                }).ToList();
+        }
+    }
+}
