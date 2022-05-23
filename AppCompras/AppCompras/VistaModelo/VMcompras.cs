21ce38157af3ad394c6027b7fcc728578dedb849
unknown <facundoromani90@gmail.com>
2022-05-23 12:32:33 -0300

    Funcionalidades para mostrar productos

--- AppCompras/AppCompras/VistaModelo/VMcompras.cs
+++ AppCompras/AppCompras/VistaModelo/VMcompras.cs
@@ -1,4 +1,6 @@
-﻿using AppCompras.VistaModelo;
+﻿using AppCompras.Datos;
+using AppCompras.Modelo;
+using AppCompras.VistaModelo;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,10 +10,12 @@ using Xamarin.Forms;
 
 namespace AppCompras.VistaModelo
 {
-    public class VMcompras:BaseViewModel
+    public class VMcompras : BaseViewModel
     {
         #region VARIABLES
         string _Texto;
+        int _index;
+        List<Mproductos> _listaproductos; // Variable lista de productos.
         #endregion
         #region CONSTRUCTOR
         public VMcompras(INavigation navigation)
@@ -25,8 +29,90 @@ namespace AppCompras.VistaModelo
             get { return _Texto; }
             set { SetValue(ref _Texto, value); }
         }
+        public List<Mproductos>Listaproductos
+        {
+            get { return _listaproductos; }
+            set { SetValue(ref _listaproductos, value); }
+        }
+
         #endregion
         #region PROCESOS
+
+        // Método async para mostrar productos.
+        public async Task Mostrarproducto(StackLayout Carrilderecha , StackLayout Carrilizquierda)
+        {
+            var funcion = new Dproductos();
+            Listaproductos = await funcion.Mostrarproductos();
+            
+            foreach (var item in Listaproductos)
+            {
+                Dibujarproductos(item, _index, Carrilderecha, Carrilizquierda);
+                _index++; // Llenar de datos el bucle en cada iteración. 
+            }
+        }
+
+        // Distribuir productos en ambas columnas.
+        public void Dibujarproductos(Mproductos item, int index, StackLayout Carrilderecha, StackLayout Carrilizquierda)
+        {
+            var _ubicacion = Convert.ToBoolean(index % 2);
+            var carril = _ubicacion ? Carrilderecha : Carrilizquierda;
+
+            var frame = new Frame
+            {
+                HeightRequest = 300,
+                CornerRadius = 10,
+                Margin = 8,
+                HasShadow = false,
+                BackgroundColor = Color.White,
+                Padding = 22,
+            };
+
+            var stack = new StackLayout
+            {
+
+            };
+
+            var image = new Image
+            {
+                Source = item.Icono,
+                HeightRequest = 150,
+                HorizontalOptions = LayoutOptions.Center,
+                Margin = new Thickness(0, 10)
+            };
+
+            var labelprecio = new Label
+            {
+                Text = "$" + item.Precio,
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 22,
+                Margin = new Thickness(0, 10),
+                TextColor = Color.Black,
+            };
+
+            var labeldescripcion = new Label
+            {
+                Text = item.Descripcion,
+                FontSize = 16,
+                TextColor = Color.Black,
+                CharacterSpacing = 1,
+            };
+
+            var labelpeso = new Label
+            {
+                Text = item.Peso,
+                FontSize = 12,
+                TextColor = Color.Black,
+                CharacterSpacing = 1,
+            };
+
+            // Contener en el stack.
+            stack.Children.Add(image);
+            stack.Children.Add(labelprecio);
+            stack.Children.Add(labeldescripcion);
+            stack.Children.Add(labelpeso);
+            frame.Content = stack;
+            carril.Children.Add(frame);
+        }
         public async Task ProcesoAsyncrono()
         {
 
